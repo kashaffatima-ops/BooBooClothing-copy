f213b132491fbd7c707f559b571c2152761fb17c
Faiz Rahman <faizrh209@gmail.com>
2024-12-08 17:41:38 +0500

    login functionality implemented

--- server/models/Staff.js
+++ server/models/Staff.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const staffSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  position: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   email: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
   },
-  phone: {
+  password: {
     type: String,
     required: true,
-    trim: true,
   },
-}, {
-  timestamps: true, 
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'staff'],
+  },
+});
+
+staffSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const Staff = mongoose.model('Staff', staffSchema);
